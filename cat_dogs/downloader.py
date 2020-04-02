504be4c8f6f95beb5ca0c1a81190b759f2bfe673
hooker5427 <2775023604@qq.com>
2020-04-02 22:29:15 +0800

    cat_dogs

---
+++ cat_dogs/downloader.py
@@ -0,0 +1,4 @@
+import  tensorflow as tf
+
+_URL = 'https://storage.googleapis.com/mledu-datasets/cats_and_dogs_filtered.zip'
+path_to_zip = tf.keras.utils.get_file('cats_and_dogs.zip', origin=_URL, extract=True)
